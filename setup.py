587dac84120e4d013cd402a63725b8afa3fa4f95
Yunus Rahbar <yunus.rahbar@rd.io>
2015-12-10 14:36:03 -0800

    auto version number

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+with open('commit_count') as commit_count_file:
+    git_commit_count = int(commit_count_file.read())
+
 setup(
     name="anime",
-    version="0.0.1",
+    version="0.0.1-%4d" % git_commit_count,
     author="Yunus Rahbar",
     description="The core app logic for MyAnimeFigures",
     long_description=read('README.md'),
