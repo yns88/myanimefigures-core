b1c1100c88032d9cadb3fbc9e5b862c5d2c4e530
Yunus Rahbar <yunus.rahbar@rd.io>
2015-12-10 17:38:23 -0800

    fix template paths

--- anime/templatetags/user_extras.py
+++ anime/templatetags/user_extras.py
@@ -3,16 +3,16 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('anime_gridobj.html')
+@register.inclusion_tag('anime/anime_gridobj.html')
 def show_anime(anime):
     return {'anime': anime}
 
 
-@register.inclusion_tag('figure_gridobj.html')
+@register.inclusion_tag('anime/figure_gridobj.html')
 def show_figure(figure):
     return {'figure': figure}
 
 
-@register.inclusion_tag('content_rows.html')
+@register.inclusion_tag('anime/content_rows.html')
 def show_content_rows(series_list, nofigs_list):
     return {'series_list': series_list, 'nofigs_list': nofigs_list}
